2a78005aa845ba96ac13b63919d69fad1285173c
Boldmax <olabode.adewunmi@mkobo.com.ng>
2021-11-05 09:33:46 +0100

    Latest update

--- src/components/pages/gallery/Gallery.jsx
+++ src/components/pages/gallery/Gallery.jsx
@@ -1,14 +1,16 @@
 import "./Gallery.css";
-import gallery1 from "../images/gallery/1.jpg";
-import gallery2 from "../images/gallery/2.jpg";
-import gallery3 from "../images/gallery/3.jpg";
-import gallery4 from "../images/gallery/4.jpg";
-import gallery5 from "../images/gallery/5.jpg";
-import gallery6 from "../images/gallery/6.jpg";
-import gallery7 from "../images/gallery/7.jpg";
-import gallery8 from "../images/gallery/8.jpg";
+import gallery from "./galleryData";
+import Carousel from 'react-elastic-carousel';
 
 const Gallery = () => {
+
+    const breakpoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 425, itemsToShow: 2 },
+        { width: 800, itemsToShow: 3 },
+        { width: 1280, itemsToShow: 4 }
+    ]
+
     return (
         <section className="gallery" id="gallerySection">
 
@@ -19,45 +21,14 @@ const Gallery = () => {
                 </div>
 
                 <div className="galleryContainer">
-                    <div className="item">
-                        <span className="title">Image Title</span>
-                        <img src={gallery1} alt="gallery" />
-                    </div>
-
-                    <div className="item">
-                        <span className="title">Image Title</span>
-                        <img src={gallery2} alt="gallery" />
-                    </div>
-
-                    <div className="item">
-                        <span className="title">Image Title</span>
-                        <img src={gallery3} alt="gallery" />
-                    </div>
-
-                    <div className="item">
-                        <span className="title">Image Title</span>
-                        <img src={gallery4} alt="gallery" />
-                    </div>
-
-                    <div className="item">
-                        <span className="title">Image Title</span>
-                        <img src={gallery5} alt="gallery" />
-                    </div>
-
-                    <div className="item">
-                        <span className="title">Image Title</span>
-                        <img src={gallery6} alt="gallery" />
-                    </div>
-
-                    <div className="item">
-                        <span className="title">Image Title</span>
-                        <img src={gallery7} alt="gallery" />
-                    </div>
-
-                    <div className="item">
-                        <span className="title">Image Title</span>
-                        <img src={gallery8} alt="gallery" />
-                    </div>
+                    <Carousel breakPoints={breakpoints}>
+                        {gallery.map((image, index) => {
+                            return <div className="item" key={index}>
+                                <span className="title">Image Title</span>
+                                <img src={image.img} alt="gallery" />
+                            </div>
+                        })}
+                    </Carousel>
                 </div>
             </div>
         </section>
---
+++ src/components/pages/gallery/galleryData.js
@@ -0,0 +1,45 @@
+import gallery1 from "../images/gallery/1.jpg";
+import gallery2 from "../images/gallery/2.jpg";
+import gallery3 from "../images/gallery/3.jpg";
+import gallery4 from "../images/gallery/4.jpg";
+import gallery5 from "../images/gallery/5.jpg";
+import gallery6 from "../images/gallery/6.jpg";
+import gallery7 from "../images/gallery/7.jpg";
+import gallery8 from "../images/gallery/8.jpg";
+
+const data = [
+    {
+        "Btn": "one",
+        img: gallery8
+    },
+    {
+        "Btn1": "two",
+        img: gallery2
+    },
+    {
+        "Btn2": "three",
+        img: gallery3
+    },
+    {
+        "Btn3": "four",
+        img: gallery6
+    },
+    {
+        "Btn3": "five",
+        img: gallery7
+    },
+    {
+        "Btn3": "six",
+        img: gallery5
+    },
+    {
+        "Btn3": "seven",
+        img: gallery4
+    },
+    {
+        "Btn3": "seven",
+        img: gallery1
+    }
+]
+
+export default data;
